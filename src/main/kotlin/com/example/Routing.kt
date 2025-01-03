b608dce42375e2ddc39874b18b4f11a18fac20b2
Margarita Skokova <367538@niuitmo.ru>
2025-01-03 04:16:10 +0300

    initial commit

---
+++ src/main/kotlin/com/example/Routing.kt
@@ -0,0 +1,23 @@
+package com.example
+
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import java.sql.Connection
+import java.sql.DriverManager
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
